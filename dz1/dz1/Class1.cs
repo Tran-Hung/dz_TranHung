da25d0e04953715e0dad9765e03b32712212093c
TranHung_71 <tranthanhhung1998@gmail.com>
2020-12-25 18:18:51 +0300

    first commit

---
+++ dz1/dz1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace dz1
+{
+    public class Metrological_system
+    {
+        public string name;
+        public int number;
+        public string address;
+
+        public Metrological_system()
+        {
+            Console.WriteLine("- Metrological_systen()");
+        }
+        public Metrological_system(string name, int number, string address)
+        {
+            this.name = name;
+            this.number = number;
+            this.address = address;
+        }
+    }
+
+    public class measuring_line : Metrological_system
+    {
+        public int number1;
+
+        public measuring_line()
+        {
+            Console.WriteLine("- measuring_line()");
+        }
+
+        public measuring_line(string name, int number, string address, int number1)
+            : base(name, number, address)
+        {
+            this.number1 = number1;
+        }
+     
+    }
+
+    public class sensor : measuring_line
+    {
+        public string type;
+        public int pressure;
+        public int viscosity;
+        public double density;
+        public double temperatura;
+        public double min_value;
+        public double max_value;
+        public string designation;
+
+        public sensor()
+        {
+            Console.WriteLine("- sensor()");
+        }
+
+        public sensor(string name, int number, string address, int number1, string type, int pressure, int viscosity
+            , double density, double temperatura, double min_value, double max_value, string designation)
+            :base(name, number, address, number1)
+        {
+            this.type = type;
+            this.pressure = pressure;
+            this.viscosity = viscosity;
+            this.density = density;
+            this.temperatura = temperatura;
+            this.min_value = min_value;
+            this.max_value = max_value;
+            this.designation = designation;
+        }
+    }
+}
---
+++ dz2/dz2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using dz1;
+
+
+namespace dz2
+{
+    class Program
+    {
+        static string SerializeAnMixObject()
+        {
+            var foo = new sensor()
+            {
+                name = "Hall",
+                number = 41,
+                address = "Honeywell International",
+                number1 = 40,
+                type = "Magnetic field",
+                pressure = 0,
+                viscosity = 0,
+                density = 0.12,
+                temperatura = 150,
+                min_value = 4.5,
+                max_value = 24,
+                designation = "A/m"
+            };
+         return JsonConvert.SerializeObject(foo);
+     
+        }
+        static sensor DeserializeAnObject()
+        {
+            string jsonString = SerializeAnMixObject();
+
+            sensor s1 = JsonConvert.DeserializeObject<sensor>(jsonString);
+            return s1;
+        }
+
+        static void Main(string[] args)
+        { 
+            sensor s1 = DeserializeAnObject();
+            Console.WriteLine("sensor: [ \n{");
+            Console.WriteLine(" name:" + s1.name);
+            Console.WriteLine(" number:" + s1.number);
+            Console.WriteLine(" address:" + s1.address);
+            Console.WriteLine(" number1:" + s1.number1);
+            Console.WriteLine(" type:" + s1.type);
+            Console.WriteLine(" pressure:" + s1.pressure);
+            Console.WriteLine(" viscosity:" + s1.viscosity);
+            Console.WriteLine(" density:" + s1.density);
+            Console.WriteLine(" temperatura:" + s1.temperatura);
+            Console.WriteLine(" min_value:" + s1.min_value);
+            Console.WriteLine(" max_value:" + s1.max_value);
+            Console.WriteLine(" designation:" + s1.designation);
+            Console.WriteLine("} \n]");
+        }
+    }
+}
